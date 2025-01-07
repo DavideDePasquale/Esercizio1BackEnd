b13b571a251c31b3b31bc9f07766356748c56dbb
Davide De Pasquale <davide.depasquale1998@gmail.com>
2025-01-07 17:31:42 +0100

    modifica

--- src/Main.java
+++ src/Main.java
@@ -5,40 +5,47 @@ import java.util.Arrays;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+     int x = 20;
+     int y = 41;
+     String str = "Sono il numero ";
+     String[] arrStr = {"uno", "due", "tre", "quattro", "cinque"};
 
-     moltiplica();
-     concatena();
-     inserisciInArray();
 
+        System.out.println("il prodotto calcolato fra i numeri " + x + " e " + y + " è uguale a " + moltiplica( x, y ));
+
+        System.out.println(concatena(str,x));
+
+        System.out.println(Arrays.toString(inserisciInArray(arrStr, "Nuovo")));
 
        }
-    public static int moltiplica() {
-        int x = 15;
-        int y = 4;
-        int sum = x * y;
-        System.out.println(sum);
-        return sum;
+    public static int moltiplica(int num1, int num2) {
+        return num1 * num2;
     }
 
-    public static void concatena(){
-        String stringa = "Ciao";
-        int numero = 2;
-        System.out.println(stringa + numero);
+    public static String concatena(String s, int n){
+        return s + n;
 
     }
-    public static void inserisciInArray(){
-        String[] array1 = {"Davide", "Carlo", "Ele", "Licia", "Mauro"};
-        String stringaAgg = "Undefined";
+    public static String[] inserisciInArray(String[] arr, String s){
         String[] arrayDiSei = new String[6];
-        arrayDiSei[0] = array1[0];
-        arrayDiSei[1] = array1[1];
-        arrayDiSei[2] = stringaAgg;
-        arrayDiSei[3] = array1[2];
-        arrayDiSei[4] = array1[3];
-        arrayDiSei[5]= array1[4];
-        for (int i = 0; i < arrayDiSei.length; i++) {
-            System.out.println(arrayDiSei[i]);
+       /* arrayDiSei[0] = arr[0];
+        arrayDiSei[1] = arr[1];
+        arrayDiSei[2] = s;
+        arrayDiSei[3] = arr[2];
+        arrayDiSei[4] = arr[3];
+        arrayDiSei[5] = arr[4]; */
+        for (int i = 0; i<arrayDiSei.length; i++){
+            if(i<2){
+                arrayDiSei[i] = arr[i];
+            } else if (i == 2){
+                arrayDiSei[i] = s;
+
+            } else {
+                arrayDiSei[i] = arr[i-1];
+            }
         }
+        return arrayDiSei;
+
 
 
     }
