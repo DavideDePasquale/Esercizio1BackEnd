fa478f87edf20a836512931125e5f996e03d76a6
Davide De Pasquale <davide.depasquale1998@gmail.com>
2025-01-07 18:00:28 +0100

    Soluzione Esercizi Giorni 1

--- src/Es3.java
+++ src/Es3.java
@@ -5,46 +5,22 @@ import static java.lang.Math.sqrt;
 public class Es3 {
     public static void main(String[] args) {
 
-      perimetroRettangolo();
-      pariDispari();
-      perimetroTriangolo();
+        System.out.println("il perimetro del rettangolo è : " + perimetroRettangolo(5,19));
+        System.out.println("il numero passato come parametro è : " + pariDispari(5));
+        System.out.println("L'area del triangolo è : " + perimetroTriangolo(2.3, 5.1, 5.4));
     }
-    public static int perimetroRettangolo(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Inserisci il primo lato del rettangolo");
-        int x = sc.nextInt();
-        System.out.println("Inserisci il secondo lato del rettangolo");
-        int y = sc.nextInt();
-        int perimetro = (x + y) * 2 ;
-        System.out.println("il perimetro del rettangolo è : " + perimetro);
-        return perimetro;
+    public static double perimetroRettangolo(double l1, double l2){
+
+        return l1 + l2 + l1 + l2;
     }
 
-    public static int pariDispari() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Inserisci qui un numero intero");
-        int a = sc.nextInt();
-        int divisibile = a % 2;
-        if (divisibile == 0) {
-            System.out.println("Il numero scelto è pari!");
-            return divisibile;
-
-        } else {
-            System.out.println("Il numero scelto è dispari!");
-            return divisibile;
+    public static int pariDispari(int num) {
+        return num % 2;
         }
-
-    }
-    public static void perimetroTriangolo(){
-        double lato1 = 10;
-         double  lato2 = 12;
-          double lato3 = 5;
-          double perimetroTri = lato1 + lato2 + lato3;
-        System.out.println("il perimetro del triangolo è : " + perimetroTri);
-         double s = (lato1 + lato2 + lato3) / 2;
-         double areaTriangolo = Math.sqrt( s * (s - lato1)* (s - lato2) * (s - lato3));
-        System.out.println("L'area del triangolo è : " + areaTriangolo);
-
+    public static double perimetroTriangolo(double l1, double l2, double l3){
+         double semiPerimetro = (l1 + l2 + l3) / 2;
+         double areaTriangolo = Math.sqrt( semiPerimetro * (semiPerimetro - l1)* (semiPerimetro - l2) * (semiPerimetro - l3));
+       return areaTriangolo;
     }
 
 
